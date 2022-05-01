6ecdad0350e2be94d60d50cd095b629ae5c38143
Your Name <you@example.com>
2022-05-01 16:26:56 +0530

    Testing length, First and Last char

--- deck_test.go
+++ deck_test.go
@@ -6,6 +6,14 @@ func TestNewDesk(t *testing.T){
     d := newDeck()
 
     if len(d) != 16 {
-        t.Errorf("Expected deck length of 16, got %d", len(d))
+        t.Errorf("Expected deck length of 16, got %v", len(d))
+    }
+
+    if d[0] != "Spades of Ace" {
+        t.Errorf("Expected first card of Spades of Ace, but got %v", d[0])
+    }
+
+    if d[len(d)-1] != "Clubs of Four" {
+        t.Errorf("Expected last card of Clubs of Four, but got %v",d[len(d)-1])
     }
 }
